ae1fddda1431dd6643d3d95af15bb660ae08faec
Bannatz <kevintoptschij12012003@gmail.com>
2024-11-01 06:40:57 +0100

    main.py: added Help command and opens the player as Default

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ from utils.tui import TUI
 import curses
 import asyncio
 
-sys.tracebacklimit = 1  # Suppress all Tracebacks.
+sys.tracebacklimit = 0  # Suppress all Tracebacks.
 
 def main():
     arg_len = len(sys.argv)
 
     if arg_len == 1:
-        print("Usage: main.py <argument>")
-        print("Commands:\nscrape [year_filter | month_filter]\nplayer - opens the music player")
-        print("Example Usage: python main.py scrape [year_filter | month_filter]")
-        return
-
+        tui = TUI()
+        curses.wrapper(tui.display_interface)
+    if sys.argv[1] == "Help":
+        print("Commands: scrape - Prints the whole 'catalog'. Optional: [year_filter | month_filter]\n
+              Help - Prints this Message\n
+              player - opens the default player")
     if sys.argv[1] == "scrape":
         # Set default values for year and month filters
         year_filter = None
@@ -28,8 +29,7 @@ def main():
                 print("Invalid Input: year_filter must be an integer.")
                 return
         # Run the scrape function with the provided filters
-        asyncio.run(Scraper().scrape("mp3", True, year_filter=year_filter, month_filter=month_filter))
-    
+        asyncio.run(Scraper().scrape("mp3", True, year_filter=year_filter, month_filter=month_filter))    
     elif sys.argv[1] == "player":
             tui = TUI()
             curses.wrapper(tui.display_interface)
